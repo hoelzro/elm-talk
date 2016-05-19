61660448435590627e351f4721c57a1d6a04fb17
Rob Hoelz <rob@hoelz.ro>
2016-05-19 18:34:40 -0500

    Update Timer.elm

--- Timer.elm
+++ Timer.elm
@@ -1,6 +1,27 @@
-import Graphics.Element exposing (Element, leftAligned)
-import Text exposing (fromString)
-import Time exposing (every, second)
+import Html exposing (Html, text)
+import Html.App as App
+import Time exposing (Time, every, second)
 
-main : Signal Element
-main = Signal.map (\t -> leftAligned <| fromString <| toString t) <| every second
+type alias Model = Time
+type Message = Tick Time
+
+init : (Model, Cmd Message)
+init = (0, Cmd.none)
+
+update : Message -> Model -> (Model, Cmd Message)
+update (Tick t) _ = (t, Cmd.none)
+
+subscriptions : Model -> Sub Message
+subscriptions _ = every second Tick
+
+view : model -> Html a
+view t = text <| toString t
+-- "text <| toString t" is shorthand for "text (toString t)"
+
+main : Program Never
+main = App.program {
+    init = init,
+    update = update,
+    subscriptions = subscriptions,
+    view = view
+  }
