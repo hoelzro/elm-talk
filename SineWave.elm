146d2bbf297256f2aacbb5dc13b65b58709bb147
Rob Hoelz <rob@hoelz.ro>
2016-05-21 15:26:42 -0500

    Update SineWave.elm for 0.17

--- SineWave.elm
+++ SineWave.elm
@@ -1,14 +1,18 @@
 -- vim:fdm=marker
 
 import Color exposing (Color, black, green, red)
-import Graphics.Collage exposing (Form, circle, collage, filled, move)
-import Graphics.Element exposing (Element)
-import Time
+import Collage exposing (Form, circle, collage, filled, move)
+import Element
+import Html exposing (Html)
+import Html.App as App
+import Time exposing (Time)
 
-type alias State = {
+type alias Model = {
   currentX   : Float,
   previousXS : List Float
 }
+
+type Message = Tick
 
 -- {{{ Configuration
 framesPerSecond : Int
@@ -35,7 +39,7 @@ previousSize = 2
 xIncrease : Float
 xIncrease = pi / 10
 
-initialState : State
+initialState : Model
 initialState = {
   currentX   = 0,
   previousXS = [] }
@@ -45,20 +49,26 @@ historySize = 10
 --}}}
 
 -- {{{ Code
+fps : Int -> (Time -> msg) -> Sub msg
+fps framesPerSecond = Time.every ((toFloat <| 1000 // framesPerSecond) * Time.millisecond)
+
 render : Color -> Int -> Float -> Form
 render color size x =
   let y = sin x
   in move (x * xScale, y * yScale) <| filled color <| circle <| toFloat size
 
-view : State -> Element
+view : Model -> Html Message
 view {currentX, previousXS} =
   let renderedCurrent  = render red currentSize currentX
       renderedPrevious = List.map (render black previousSize) previousXS
   in
-    collage width height <| renderedCurrent :: renderedPrevious
+    Element.toHtml <| collage width height <| renderedCurrent :: renderedPrevious
 
-update : Float -> State -> State
-update _ {currentX, previousXS} =
+init : (Model, Cmd Message)
+init = (initialState, Cmd.none)
+
+update : Message -> Model -> (Model, Cmd Message)
+update Tick {currentX, previousXS} =
   let maxX = (toFloat width) / 2.0
       minX = 0 - maxX
       newX =
@@ -66,14 +76,19 @@ update _ {currentX, previousXS} =
           then minX / xScale
           else currentX + xIncrease
   in
-    {
+    ({
       currentX   = newX,
       previousXS = currentX :: List.take historySize previousXS
-    }
+    }, Cmd.none)
 
-main : Signal Element
-main =
-  Signal.map view
-    <| Signal.foldp update initialState
-    <| Time.fps framesPerSecond
+subscriptions : Model -> Sub Message
+subscriptions _ = fps framesPerSecond <| always Tick
+
+main : Program Never
+main = App.program {
+    init = init,
+    view = view,
+    update = update,
+    subscriptions = subscriptions
+  }
 -- }}}
